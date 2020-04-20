d7fef58e974e2216a14c5023f4036abd61d6fa2a
Weslley <wvictor07@gmail.com>
2020-04-20 21:49:22 +0100

    improved find algorithm

--- index.js
+++ index.js
@@ -6,23 +6,17 @@ const request = require('request')
 const functions = require('./lib/functions.js')
 const PORT = process.env.PORT || 5000
 
-
 const server = http.createServer((req, res)=>{
     res.setHeader('Content-Type', 'application/json')
-    //instantiating date class for diff variable
+    //instantiating date object to measure time it took to get image
     let d1 = new Date()
     let timeInit = d1.getTime()
-
     let urlString = url.parse(req.url)
     switch(urlString.pathname){
-
         case '/getBookCover':
-            console.log(querystring.parse(urlString.query), urlString.pathname)
-
             if(querystring.parse(urlString.query)['bookTitle'] || querystring.parse(urlString.query)['authorName']){
                 let bookTitle = querystring.parse(urlString.query)['bookTitle'] || ''
                 let authorName = querystring.parse(urlString.query)['authorName'] || ''
-
                 let query = `${bookTitle.replace(' ', '+')} ${authorName.replace('', '+')}`
                 //making request to google to get book's goodreads page
                 request(`https://www.google.com/search?q=${query}+goodreads&sourceid=chrome&ie=UTF-8`, (err, response, body)=>{
@@ -32,28 +26,25 @@ const server = http.createServer((req, res)=>{
                     let goodreadsLink = functions.getLinkGoogle(body)
                     //Making request to goodreads to get the book cover image tag
                     request(goodreadsLink, (err, response, body)=>{
-                        if(err){
+                        if(err)
                             res.end(JSON.stringify({error: err}))
-                        }
+                        
                         let bookCoverLink = functions.getLinkGoodreads(body)
-                        //instantiating new date for comparison with the one I instantiated when request was made
+                        //instantiating new date object to get time finished
                         let d2 = new Date()
                         let timeEnd = d2.getTime()
                         let diff =  timeEnd-timeInit
                         //sending json response
                         res.end(JSON.stringify({status: 'success', delay:`${diff/1000} seconds` ,method: 'getBookCover', bookCoverUrl: bookCoverLink}))
-                    })
-                    
-                })
-                
+                    })                  
+                })        
             }
-            else{//if no query is inserted
+            else//if no query is inserted
                 res.end(JSON.stringify({status: 'failed', error: 'Please insert options for search.'}))
-            }
+            
             break
         default: 
-            res.end(JSON.stringify({status: 'failed', error: 'Method not suported yet.'}))
-            
+            res.end(JSON.stringify({status: 'failed', error: 'Method not suported yet.'}))   
     }
 })
 
