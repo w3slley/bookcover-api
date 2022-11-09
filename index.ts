c21575fe6a2c49a7640d981f7c8a8b3dfd885085
Weslley <wvictor07@gmail.com>
2022-11-09 21:18:28 +0000

    added types to req and res objects and installed node types

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
-const PORT = process.env.PORT || 8000;
-const app = require('express')();
+const PORT = process.env.PORT || 2000;
+import express, { Request, Response, NextFunction } from 'express';
+const app = express();
 require('dotenv').config();
 
-app.use((req, res, next) => {
+app.use((req: Request, res: Response, next: NextFunction) => {
     res.setHeader('Content-Type', 'application/json');
     next();
 })
 
 app.use('/bookcover', require('./routes/bookcover'));
 
-app.get('*', (req, res) => {
+app.get('*', (req: Request, res: Response) => {
     res.setHeader('Content-Type', 'application/json');
-    res.status(400).json({status: 'failed', error: 'Method not suported yet.'});
+    res.status(400).json({ status: 'failed', error: 'Method not suported yet.' });
 });
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}!`);
 })
