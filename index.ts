1bd612e1a765d1fca779a8b6e2b284fd43eb98eb
Weslley Vieira <w3slley@pm.me>
2024-03-23 22:40:52 +0000

    Improve code

--- index.ts
+++ index.ts
@@ -1,18 +1,20 @@
-const PORT = process.env.PORT || 2000;
-import express, { Request, Response, NextFunction } from 'express';
-import HttpException from './exceptions/HttpException';
-import { METHOD_NOT_SUPPORTED } from './helpers/messages';
-import errorHandler from './middlewares/errorHandler';
-import jsonHeader from './middlewares/headers';
-const app = express();
-require('dotenv').config();
+import express, { Request, Response, NextFunction } from "express";
+import HttpException from "./exceptions/HttpException";
+import { METHOD_NOT_SUPPORTED } from "./helpers/messages";
+import errorHandler from "./middlewares/errorHandler";
+import jsonHeader from "./middlewares/headers";
+require("dotenv").config();
 
+const app = express();
+const PORT = process.env.PORT || 2000;
 
 app.use(jsonHeader);
-app.use('/bookcover', require('./routes/bookcover'));
-app.get('/*', (req: Request, res: Response, next: NextFunction) => {
+app.use("/bookcover", require("./routes/bookcover"));
+
+app.get("/*", (req: Request, res: Response, next: NextFunction) => {
   next(new HttpException(400, METHOD_NOT_SUPPORTED));
 });
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
