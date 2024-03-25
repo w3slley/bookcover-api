e5a07bec62ec422eceaebea1ca441a553e626e4c
Weslley Vieira <w3slley@pm.me>
2024-03-25 23:53:17 +0000

    Improve folder structure and improve home.go handler

--- handlers/home.go
+++ handlers/home.go
@@ -8,16 +8,21 @@ import (
 
 const METHOD_NOT_SUPPORTED string = "Method not suported yet."
 
-func Home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
+func BuildErrorResponse(w http.ResponseWriter) []byte {
   jsonObj :=map[string] string { "error": METHOD_NOT_SUPPORTED}
   res, err := json.Marshal(jsonObj)
   if err != nil {
     fmt.Println(err)
-  } 
+  }
 
   w.WriteHeader(http.StatusBadRequest)
-  w.Write(res)
+  return res
 }
 
+func Home(w http.ResponseWriter, r *http.Request) {
+  w.Header().Set("Content-Type", "application/json")
+   
+  res := BuildErrorResponse(w)
+  w.Write(res)
+}
 
