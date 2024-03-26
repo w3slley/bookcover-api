c2adaff93413e6b29a0d859c260d0ed0eceee1f5
Weslley Vieira <w3slley@pm.me>
2024-03-26 00:11:48 +0000

    Add http exception struct

--- handlers/home.go
+++ handlers/home.go
@@ -8,21 +8,28 @@ import (
 
 const METHOD_NOT_SUPPORTED string = "Method not suported yet."
 
-func BuildErrorResponse(w http.ResponseWriter) []byte {
-  jsonObj :=map[string] string { "error": METHOD_NOT_SUPPORTED}
+type HttpException struct {
+  statusCode int
+  message string
+}
+
+func BuildErrorResponse(w http.ResponseWriter, ex HttpException) []byte {
+  jsonObj := map[string] string { "error": ex.message }
   res, err := json.Marshal(jsonObj)
   if err != nil {
     fmt.Println(err)
   }
 
-  w.WriteHeader(http.StatusBadRequest)
+  w.WriteHeader(ex.statusCode)
   return res
 }
 
-func Home(w http.ResponseWriter, r *http.Request) {
-  w.Header().Set("Content-Type", "application/json")
-   
-  res := BuildErrorResponse(w)
-  w.Write(res)
+func Home(writer http.ResponseWriter, request *http.Request) {
+  writer.Header().Set("Content-Type", "application/json")
+  httpException := HttpException{  
+    statusCode: http.StatusBadRequest, message: METHOD_NOT_SUPPORTED,
+  } 
+  response := BuildErrorResponse(writer, httpException)
+  writer.Write(response)
 }
 
