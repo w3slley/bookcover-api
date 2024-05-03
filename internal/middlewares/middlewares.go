bacb9da1ce2cf458905cf4ec9125dce67dfb4488
Wes <wvictor07@gmail.com>
2024-05-03 22:47:03 +0100

    New: Add middleware chain and update ROUTE_NOT_SUPPORTED message (#23)

--- internal/middlewares/middlewares.go
+++ internal/middlewares/middlewares.go
@@ -2,9 +2,43 @@ package middlewares
 
 import "net/http"
 
-func JsonHeaderMiddleware(f http.HandlerFunc) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		f(w, r)
+type Middleware func(http.HandlerFunc) http.HandlerFunc
+
+func Chain(f http.HandlerFunc, middlewares ...Middleware) http.HandlerFunc {
+	size := len(middlewares)
+	for i := 0; i < size; i++ {
+		f = middlewares[size-1-i](f)
+	}
+	return f
+}
+
+func HttpMethod(method string) Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			switch r.Method {
+			case method:
+				f(w, r)
+			default:
+				http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			}
+		}
+	}
+}
+
+func JsonHeaderMiddleware() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			f(w, r)
+		}
+	}
+}
+
+func CorsHeaderMiddleware() Middleware {
+	return func(f http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Access-Control-Allow-Origin", "*")
+			f(w, r)
+		}
 	}
 }
--- main.go
+++ main.go
@@ -1,19 +1,35 @@
 package main
 
 import (
-	"bookcover-api/internal/middlewares"
-	"bookcover-api/internal/routes"
 	"fmt"
 	"net/http"
 	"strconv"
+
+	"bookcover-api/internal/middlewares"
+	"bookcover-api/internal/routes"
 )
 
 const PORT int = 8000
 
 func main() {
-	http.HandleFunc("/", middlewares.JsonHeaderMiddleware(routes.Home))
-	http.HandleFunc("/bookcover", middlewares.JsonHeaderMiddleware(routes.BookcoverSearch))
-	http.HandleFunc("/bookcover/{isbn}", middlewares.JsonHeaderMiddleware(routes.BookcoverByIsbn))
+	http.HandleFunc("/", middlewares.Chain(
+		routes.Home,
+		middlewares.JsonHeaderMiddleware(),
+		middlewares.CorsHeaderMiddleware(),
+	))
+	http.HandleFunc("/bookcover", middlewares.Chain(
+		routes.BookcoverSearch,
+		middlewares.HttpMethod("GET"),
+		middlewares.JsonHeaderMiddleware(),
+		middlewares.CorsHeaderMiddleware(),
+	))
+
+	http.HandleFunc("/bookcover/{isbn}", middlewares.Chain(
+		routes.BookcoverByIsbn,
+		middlewares.HttpMethod("GET"),
+		middlewares.JsonHeaderMiddleware(),
+		middlewares.CorsHeaderMiddleware(),
+	))
 
 	fmt.Printf("Server listening at port %d 🚀\n", PORT)
 	http.ListenAndServe(":"+strconv.Itoa(PORT), nil)
