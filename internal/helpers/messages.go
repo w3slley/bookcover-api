7a9aee9dfe6d126159aef14aea6e3dfea60d5ed1
Weslley Vieira <w3slley@pm.me>
2024-05-03 22:42:32 +0100

    New: Add middleware chain and update ROUTE_NOT_SUPPORTED message

--- internal/helpers/messages.go
+++ internal/helpers/messages.go
@@ -1,6 +1,6 @@
 package helpers
 
-const METHOD_NOT_SUPPORTED string = "Method not suported yet."
+const ROUTE_NOT_SUPPORTED string = "Route not yet suported."
 const BOOKCOVER_NOT_FOUND string = "Bookcover was not found."
 const INVALID_ISBN string = "Invalid ISBN (please use ISBN-13)."
 const ERROR_READING_BODY string = "An error occurred while reading body of the request."
