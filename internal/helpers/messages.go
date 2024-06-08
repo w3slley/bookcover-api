f68e98bd764c91dc9d88d46423c3020ff27d4118
Weslley Vieira <w3slley@pm.me>
2024-06-08 16:25:02 +0100

    Improved route not supported message

--- internal/helpers/messages.go
+++ internal/helpers/messages.go
@@ -1,6 +1,6 @@
 package helpers
 
-const ROUTE_NOT_SUPPORTED string = "Route not yet suported."
+const ROUTE_NOT_SUPPORTED string = "Route is not suported yet."
 const BOOKCOVER_NOT_FOUND string = "Bookcover was not found."
 const INVALID_ISBN string = "Invalid ISBN (please use ISBN-13)."
 const ERROR_READING_BODY string = "An error occurred while reading body of the request."
